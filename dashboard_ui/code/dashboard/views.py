f509096e126314135c0f4852662868d190b8ba7d
Greg-Hawkridge <ghawkridge@gmail.com>
2025-01-14 10:55:17 +0000

    Patch - fix location name and start-up order
    - also adds connection error page in case that is still an issue

--- dashboard_ui/code/dashboard/views.py
+++ dashboard_ui/code/dashboard/views.py
@@ -3,32 +3,35 @@ from django.conf import settings
 import requests
 
 def location_dash(request):
-    response = requests.get("http://job-db.docker.local/state/jobs")
-    state =  response.json()
-    response = requests.get("http://job-db.docker.local/state/locations")
-    locations_raw = response.json()
-    locations = []
+    try:
+        response = requests.get("http://job-db.docker.local/state/jobs")
+        state =  response.json()
+        response = requests.get("http://job-db.docker.local/state/locations")
+        locations_raw = response.json()
+        locations = []
 
-    fields_shown = settings.LOC_FIELDS_SHOWN
-    field_names = settings.LOC_FIELD_NAMES
-    fields = {'shown':fields_shown,'names':field_names}
+        fields_shown = settings.LOC_FIELDS_SHOWN
+        field_names = settings.LOC_FIELD_NAMES
+        fields = {'shown':fields_shown,'names':field_names}
 
-    for entry in locations_raw:
-        locations += [ entry['name'] ]
+        for entry in locations_raw:
+            locations += [ entry['name'] ]
 
-    return render(
-            request,
-            'locations.html',
-            {
-                'fields':fields,
-                'locations':locations,
-                'jobstate':state,
-                'sort':'descending' if settings.SORT_ORDER_DESCENDING else 'ascending',
-                'show_duration':'true' if settings.SHOW_DURATION else 'false',
-                'id_as_link':'true' if settings.ID_AS_LINK else 'false',
-                'id_link_template':settings.LINK_TEMPLATE,
-            }
-        )
+        return render(
+                request,
+                'locations.html',
+                {
+                    'fields':fields,
+                    'locations':locations,
+                    'jobstate':state,
+                    'sort':'descending' if settings.SORT_ORDER_DESCENDING else 'ascending',
+                    'show_duration':'true' if settings.SHOW_DURATION else 'false',
+                    'id_as_link':'true' if settings.ID_AS_LINK else 'false',
+                    'id_link_template':settings.LINK_TEMPLATE,
+                }
+            )
+    except ConnectionRefusedError:
+        return render(request,"error.html",{"reason":"Can't reach database right now - try refreshing in a few seconds"})
 
 def job_dash(request):
     response = requests.get("http://job-db.docker.local/state/jobs")
